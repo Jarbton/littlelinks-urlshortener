484589c7c5c682bdd7dc2b7d69682ddf0183d0de
Jacob Barton <j.barton.mail@googlemail.com>
2019-07-30 14:55:31 +0100

    New URL Model Field Change
    - Alter the maximum size of the slugfield in Shorturl model to accomodate new three word URLs.

--- littlelinksite/models.py
+++ littlelinksite/models.py
@@ -4,7 +4,7 @@ from __future__ import unicode_literals
 from django.db import models
 
 class Shorturl(models.Model):
-    new_url_id = models.SlugField(max_length=6, primary_key=True)
+    new_url_id = models.SlugField(max_length=50, primary_key=True)
     orig_url = models.URLField(max_length=2083) # IE Maximum URL length
     no_clicks = models.IntegerField(default = 0)
     pub_date = models.DateTimeField('date published', auto_now=True)
